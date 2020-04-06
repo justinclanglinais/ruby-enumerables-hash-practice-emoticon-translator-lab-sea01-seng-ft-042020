4a815010031fb268b58ec75e96291cb2a0166262
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 01:48:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,6 @@ require 'yaml'
 def load_library(path)
   emo_ref = YAML.load_file(path)
   lib = {}
-  emo_ref.each do | name , array |
-    lib[name] = {"english": array(0)}
-    lib[name] = {"japanese": array(1)}
-  end
   p emo_ref
 end
 
