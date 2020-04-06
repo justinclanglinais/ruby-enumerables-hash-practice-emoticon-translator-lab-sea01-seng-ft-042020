0fa61905bf24a7b0b75ba6e890e947cd9e9b9dd1
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 01:04:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,12 @@ require 'yaml'
 
 def load_library(path)
   emo_ref = YAML.load_file(path)
-  p emo_ref
+  library = {}
+  emo_ref.each do | name , face_array |
+    library[:name][:english]
+    library[:name][:japanese]
+  end
+  p library 
 end
 
 def get_japanese_emoticon
