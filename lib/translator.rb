aebea705c02233d1dad85fdfe901226b17735671
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 00:52:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,9 @@
 # require modules here
 require "yaml"
 emo_ref = YAML.load_file('lib/emoticons.yml')
+def load_library(path)
+  emo_ref = YAML.load_file('path')
+end
 
 def load_library
   # code goes here
