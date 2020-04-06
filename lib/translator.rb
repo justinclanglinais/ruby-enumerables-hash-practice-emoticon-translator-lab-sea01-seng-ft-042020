fc1e3da5fe5d07d1afbe0249a913c4a1bbf252b8
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 03:03:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(path,emo_jp)
   lib = load_library( path )
   lib.each do | name , value |
     if lib[name][:japanese] == emo_jp
-      p name
+      p value
     end
   end
 end
