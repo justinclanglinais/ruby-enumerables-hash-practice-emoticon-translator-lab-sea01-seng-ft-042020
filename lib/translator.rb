3e0dda9970e5c0dfff8a3a18d77ad14304410195
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-06 00:37:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,47 +1,4 @@
 # require modules here
-emoticons = {
-  :angel => {
-    :english =>
-  }
-  - "O:)"      
-  - "☜(⌒▽⌒)☞"
-angry:
-  - ">:("
-  - "ヽ(ｏ`皿′ｏ)ﾉ"
-bored:
-  - ":O"
-  - "(ΘεΘ;)"
-confused:
-  - "%)"
-  - "(゜.゜)"
-embarrassed:
-  - ":$" 
-  - "(#^.^#)"
-fish:
-  - "><>"
-  - ">゜))))彡"
-glasses:
-  - "8D"
-  - "(^0_0^)"
-grinning:
-  - "=D"
-  - "(￣ー￣)"
-happy:
-  - ":)"
-  - "(＾ｖ＾)"
-kiss:
-  - ":*"
-  - "(*^3^)/~☆"
-sad:
-  - ":'("
-  - "(Ｔ▽Ｔ)"
-surprised:
-  - ":o"
-  - "o_O"
-wink:
-  - ";)"
-  - "(^_-)"
-}
 
 def load_library
   # code goes here
